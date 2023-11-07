5ae3a0ae0fa4ec7ffd4316c3aecf60bdfdce3720
a-rcane <73655772+a-rcane@users.noreply.github.com>
2023-11-07 17:22:30 +0530

    Fix

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from base_scraper import BaseScraper
 
-if __name__ == '__main__':
-    datanyze = ['https://www.datanyze.com/industries/software', 'div', 'trending-companies-cards-wrapper']
-    scraper = BaseScraper()
-    datanyze_data = scraper.scrape_site(datanyze[0], datanyze[1])
 
